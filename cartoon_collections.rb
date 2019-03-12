73ffe848bafb6c7d0dad947fc0f2417d4570f001
Brian Tobal <brian.tobal@gmail.com>
2019-03-12 10:39:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer)
+  planeteer.map do |x|
+    x.capitalize + "!"
+  end 
 end
 
 def long_planeteer_calls# code an argument here
